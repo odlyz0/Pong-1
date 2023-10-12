6ab5100431e8d82932113c6eefd6ddf66278cd6a
Cidra Sawed <Odlyz0@hotmail.com>
2023-10-12 10:55:45 +0200

    adf
    d

--- Assets/script/Bal.cs
+++ Assets/script/Bal.cs
@@ -1,6 +1,7 @@
  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Bal : MonoBehaviour
 {
@@ -8,13 +9,36 @@ public class Bal : MonoBehaviour
     public float Yposition = 0f;
     public float xSpeed;
     public float ySpeed;
+    public TMP_Text scoreField;
+    private int leftScore = 0;
+    private int rightScore = 0;
+    public int topScore = 10;
+    private void resetBall(string leftOrRight)
+    {
+        Xposition = 0f;
+        Yposition = 0f;
+        if (leftOrRight == "left")
+        {
+
+            xSpeed = 4f;
+            ySpeed = 4f;
+        }
+        else if (leftOrRight=="right")
+        {
+
+            xSpeed = -4f;
+            ySpeed = 4f;
+        }
+        scoreField.text = leftScore + " - " + rightScore;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
 
-        xSpeed = 1f;
-        ySpeed = 1f;    
+        xSpeed = 4f;
+        ySpeed = 4f;
+        scoreField.text = " 0 - 0";
 
     }
 
@@ -24,6 +48,22 @@ public class Bal : MonoBehaviour
         Xposition += xSpeed * Time.deltaTime;
         Yposition += ySpeed * Time.deltaTime;
         transform.position = new Vector3(Xposition, Yposition, 0);
+        if (leftScore >= topScore)
+        {
+            scoreField.text = "left player has won!";
+            xSpeed = 0f;
+            ySpeed = 0f;
+            Xposition = 0f;
+            Yposition = 0f;
+        }
+        else if (rightScore>= topScore)
+        {
+            scoreField.text = "Right player has won!";
+            xSpeed = 0f;
+            ySpeed = 0f;
+            Xposition = 0f;
+            Yposition = 0f;
+        }
     }
 
 
@@ -33,9 +73,20 @@ public class Bal : MonoBehaviour
         {
             ySpeed = ySpeed * -1;
         }
-        if (collision.gameObject.CompareTag("Vertical"))
+       
+        else if (collision.gameObject.CompareTag("leftWall"))
         {
-           xSpeed = xSpeed * -1;
+            rightScore++;
+            resetBall("left");
+        }
+        else if (collision.gameObject.CompareTag("rightWall"))
+        {
+            leftScore++;
+            resetBall("right");
+        }
+        else if (collision.gameObject.CompareTag("Player"))
+        {
+            xSpeed = xSpeed * -1;
         }
     }
 }
